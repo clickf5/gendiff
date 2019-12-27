edee550ebc87119b15234beb661f5c09f9cd4de5
clickf5 <paninhome@gmail.com>
2019-12-27 08:28:08 +0300

    add -f arguments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import program from 'commander';
 
 export default () => {
   program.version('0.1.0')
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format');
 
   program.parse(process.argv);
 };
