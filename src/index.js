7bcaa325c77b35c18bdddd3c1f3728cb67444c9f
clickf5 <paninhome@gmail.com>
2019-12-27 09:09:18 +0300

    add action and arguments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import program from 'commander';
 export default () => {
   program.version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'Output format');
+    .option('-f, --format [type]', 'Output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig, option) => {
+      console.log(option);
+    });
 
   program.parse(process.argv);
 };
