70701b1ebbb7ac8cbb176ac892a8baabf183f124
clickf5 <paninhome@gmail.com>
2020-01-13 08:25:30 +0300

    add new test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,11 +2,20 @@ import path from 'path';
 import fs from 'fs';
 import gendiff from '../src';
 
-test('gendiff', () => {
-  const beforePath = path.resolve(`${__dirname}/../__fixtures__/before.json`);
-  const afterPath = path.resolve(`${__dirname}/../__fixtures__/after.json`);
+describe('gendiff', () => {
+  const beforeJSONPath = path.resolve(`${__dirname}/../__fixtures__/before.json`);
+  const afterJSONPath = path.resolve(`${__dirname}/../__fixtures__/after.json`);
+
+  const beforeYAMLPath = path.resolve(`${__dirname}/../__fixtures__/before.yml`);
+  const afterYAMLPath = path.resolve(`${__dirname}/../__fixtures__/after.yml`);
 
   const result = fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`, 'utf8').trim();
 
-  expect(gendiff(beforePath, afterPath)).toEqual(result);
+  test('json', () => {
+    expect(gendiff(beforeJSONPath, afterJSONPath)).toEqual(result);
+  });
+
+  test('yaml', () => {
+    expect(gendiff(beforeYAMLPath, afterYAMLPath)).toEqual(result);
+  });
 });
