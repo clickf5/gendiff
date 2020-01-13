aae2bce73a26504c864f0149b830dde8a05ac8fe
clickf5 <paninhome@gmail.com>
2020-01-13 08:14:50 +0300

    add new test

--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -3,10 +3,14 @@ import fs from 'fs';
 import getParser from '../src/parsers';
 
 describe('parsers', () => {
-  const after = path.resolve(`${__dirname}/../__fixtures__/after.json`);
-  const afterJSON = JSON.parse(fs.readFileSync(after));
+  const afterJSONPath = path.resolve(`${__dirname}/../__fixtures__/after.json`);
+  const afterYAMLPath = path.resolve(`${__dirname}/../__fixtures__/after.yml`);
+  const afterJSON = JSON.parse(fs.readFileSync(afterJSONPath));
 
   test('json', () => {
-    expect(getParser()(after)).toEqual(afterJSON);
+    expect(getParser()(afterJSONPath)).toEqual(afterJSON);
+  });
+  test('yaml', () => {
+    expect(getParser()(afterYAMLPath)).toEqual(afterJSON);
   });
 });
