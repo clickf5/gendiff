6b5f36c68588e1a21008a9f2f9df5ec09c99f09b
clickf5 <paninhome@gmail.com>
2020-01-15 09:35:31 +0300

    use test.each in jest

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,27 +2,12 @@ import path from 'path';
 import fs from 'fs';
 import gendiff from '../src';
 
-describe('gendiff', () => {
-  const beforeJSONPath = path.resolve(`${__dirname}/../__fixtures__/before.json`);
-  const afterJSONPath = path.resolve(`${__dirname}/../__fixtures__/after.json`);
+const extensions = ['json', 'yml', 'ini'];
 
-  const beforeYAMLPath = path.resolve(`${__dirname}/../__fixtures__/before.yml`);
-  const afterYAMLPath = path.resolve(`${__dirname}/../__fixtures__/after.yml`);
-
-  const beforeINIPath = path.resolve(`${__dirname}/../__fixtures__/before.ini`);
-  const afterINIPath = path.resolve(`${__dirname}/../__fixtures__/after.ini`);
-
+test.each(extensions)('%s gendiff', (extension) => {
+  const beforeFilePath = path.resolve(`${__dirname}/../__fixtures__/before.${extension}`);
+  const afterFilePath = path.resolve(`${__dirname}/../__fixtures__/after.${extension}`);
   const result = fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`, 'utf8').trim();
 
-  test('json', () => {
-    expect(gendiff(beforeJSONPath, afterJSONPath)).toEqual(result);
-  });
-
-  test('yaml', () => {
-    expect(gendiff(beforeYAMLPath, afterYAMLPath)).toEqual(result);
-  });
-
-  test('ini', () => {
-    expect(gendiff(beforeINIPath, afterINIPath)).toEqual(result);
-  });
+  expect(gendiff(beforeFilePath, afterFilePath)).toEqual(result);
 });
