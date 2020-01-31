a94b2d5f95db67b332845a7ed06cf1b2d1a830b2
clickf5 <paninhome@gmail.com>
2020-01-31 08:36:05 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -5,40 +5,87 @@ import { version } from '../package.json';
 
 const parser = getParser();
 
+const getUniqKeys = (obj1, obj2) => [...Object.keys(obj1), ...Object.keys(obj2)]
+  .filter((value, index, array) => array.indexOf(value) === index);
+
 const getAST = (beforeObject, afterObject) => {
-  const keys = [...Object.keys(beforeObject), ...Object.keys(afterObject)]
-    .filter((value, index, array) => array.indexOf(value) === index);
-  // console.log(keys);
+  const keys = getUniqKeys(beforeObject, afterObject);
 
   const actions = [
     {
       name: 'wasAdded',
       check: (key) => _.has(afterObject, key) && !_.has(beforeObject, key),
+      getValue: (key) => afterObject[key],
+      getNewValue: () => '',
+      getChildren: () => [],
     },
     {
       name: 'wasDeleted',
       check: (key) => _.has(beforeObject, key) && !_.has(afterObject, key),
+      getValue: (key) => beforeObject[key],
+      getNewValue: () => '',
+      getChildren: () => [],
+    },
+    {
+      name: 'hasChildren',
+      check: (key) => (beforeObject[key] instanceof Object && afterObject[key] instanceof Object),
+      getValue: (key) => beforeObject[key],
+      getNewValue: (key) => afterObject[key],
+      getChildren: (key) => getAST(beforeObject[key], afterObject[key]),
     },
     {
       name: 'wasChanged',
       check: (key) => beforeObject[key] !== afterObject[key],
+      getValue: (key) => beforeObject[key],
+      getNewValue: (key) => afterObject[key],
+      getChildren: () => [],
     },
     {
       name: 'notChanged',
       check: (key) => beforeObject[key] === afterObject[key],
+      getValue: (key) => beforeObject[key],
+      getNewValue: () => '',
+      getChildren: () => [],
     },
   ];
 
   return keys.reduce((acc, key) => {
     const action = actions.find((item) => item.check(key));
-    const value = beforeObject[key] || afterObject[key];
-    const newValue = (action.name === 'wasChanged') ? afterObject[key] : '';
 
     return [...acc, {
-      action: action.name, key, value, newValue,
+      action: action.name,
+      key,
+      value: action.getValue(key),
+      newValue: action.getNewValue(key),
+      children: action.getChildren(key),
     }];
   }, []);
-  // console.log(ast);
+};
+
+const stringify = (value) => ((value instanceof Object)
+  ? `{\n${Object.keys(value).map((key) => `${key}: ${value[key]}`).join('\n')}}` : value);
+
+const render = (ast, level = 1) => {
+  const signs = {
+    wasAdded: '+',
+    wasDeleted: '-',
+    hasChildren: ' ',
+    wasChanged: ' ',
+    notChanged: ' ',
+  };
+
+  const str = ast.map((current) => {
+    const indent = (level === 1) ? ' '.repeat(2) : ' '.repeat(2 * (level + 1));
+    const value = (current.action === 'hasChildren')
+      ? render(current.children, level + 1) : stringify(current.value);
+    const newValue = stringify(current.newValue);
+    const sign = signs[current.action];
+    const line = (current.action === 'wasChanged')
+      ? `${indent}- ${current.key}: ${value}\n${indent}+ ${current.key}: ${newValue}` : `${indent}${sign} ${current.key}: ${value}`;
+    return line;
+  });
+
+  return `{\n${str.join('\n')}\n}`;
 };
 
 const gendiff = (beforeConf, afterConf) => {
@@ -47,26 +94,13 @@ const gendiff = (beforeConf, afterConf) => {
 
   const ast = getAST(beforeObject, afterObject);
 
-  const signs = {
-    wasAdded: '+',
-    wasDeleted: '-',
-    wasChanged: ' ',
-    notChanged: ' ',
-  };
-
-  const str = ast.reduce((acc, current) => {
-    const line = (current.action === 'wasChanged') ? [`${' '.repeat(2)}- ${current.key}: ${current.value}`, `${' '.repeat(2)}+ ${current.key}: ${current.newValue}`] : [`${' '.repeat(2)}${signs[current.action]} ${current.key}: ${current.value}`];
-    return [...acc, ...line];
-  }, []);
-  // console.log(str);
-
-  return ['{', ...str, '}'].join('\n');
+  return render(ast);
 };
 
 export const cli = () => {
   program.version(version)
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'Output format')
+    .option('-f, --format [type]', 'Output format', 'json')
     .arguments('<firstConfig> <secondConfig>')
     .action((firstConfig, secondConfig) => {
       console.log(gendiff(firstConfig, secondConfig));
