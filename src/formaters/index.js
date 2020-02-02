c31f975d87a18170bce337f1848b55504473f3be
clickf5 <paninhome@gmail.com>
2020-02-02 23:04:06 +0300

    add plain formatter

--- src/formaters/index.js
+++ src/formaters/index.js
@@ -1,7 +1,9 @@
 import renderTree from './renderTree';
+import renderPlain from './renderPlain';
 
 const formaters = {
   tree: renderTree,
+  plain: renderPlain,
 };
 
 export default (format) => formaters[format];
---
+++ src/formaters/renderPlain.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+
+const isCompexValue = (val) => (_.isObject(val)) ? '[complex value]' : val;
+
+const renderPlain = (ast, parents = '') => {
+  const mapped = ast.map((current) => {
+    const propertyName = (parents === '') ? current.key : `${parents}.${current.key}`;
+    switch (current.state) {
+      case 'added':
+        return `Property '${propertyName}' was added with value ${isCompexValue(current.value)}`;
+      case 'deleted':
+        return `Property '${propertyName}' was deleted`;
+      case 'hasChildren':
+        return renderPlain(current.children, current.key);
+      case 'changed':
+        return `Property '${propertyName}' was changed from ${current.value} to ${current.newValue}`;
+      case 'unchanged':
+        return '';
+      default:
+        throw new Error(`Invalid state '${current.state}'`);
+    }
+  });
+
+  return mapped.filter((item) => item !== '').join('/n');
+};
+
+export default renderPlain;
