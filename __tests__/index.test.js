45c7d86268ff3f53d1b9e8b07471ae3004278a6d
clickf5 <paninhome@gmail.com>
2020-02-06 10:25:17 +0300

    test refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,23 +3,14 @@ import fs from 'fs';
 import gendiff from '../src';
 
 const extensions = ['json', 'yml', 'ini'];
+const outputs = ['tree', 'plain', 'json'];
 
 describe.each(extensions)('%s gendiff', (extension) => {
   const beforeFilePath = path.resolve(`${__dirname}/../__fixtures__/before.${extension}`);
   const afterFilePath = path.resolve(`${__dirname}/../__fixtures__/after.${extension}`);
 
-  test('tree', () => {
-    const result = fs.readFileSync(`${__dirname}/../__fixtures__/result_tree.txt`, 'utf8').trim();
-    expect(gendiff(beforeFilePath, afterFilePath, 'tree')).toEqual(result);
-  });
-
-  test('plain', () => {
-    const result = fs.readFileSync(`${__dirname}/../__fixtures__/result_plain.txt`, 'utf8').trim();
-    expect(gendiff(beforeFilePath, afterFilePath, 'plain')).toEqual(result);
-  });
-
-  test('json', () => {
-    const result = fs.readFileSync(`${__dirname}/../__fixtures__/result_json.txt`, 'utf8').trim();
-    expect(gendiff(beforeFilePath, afterFilePath, 'json')).toEqual(result);
+  test.each(outputs)('%s', (output) => {
+    const result = fs.readFileSync(`${__dirname}/../__fixtures__/result_${output}.txt`, 'utf8').trim();
+    expect(gendiff(beforeFilePath, afterFilePath, output)).toEqual(result);
   });
 });
