20e01280d2e2326e62f39ac8d49f30e7fb06fd78
clickf5 <paninhome@gmail.com>
2020-02-06 12:26:42 +0300

    add replacer

--- src/formaters/renderJSON.js
+++ src/formaters/renderJSON.js
@@ -1,3 +1,23 @@
 import _ from 'lodash';
 
-export default (ast) => JSON.stringify(ast);
+const typeActions = [
+  {
+    check: (val) => _.isBoolean(val),
+    action: (val) => val,
+  },
+  {
+    check: (val) => !_.isNaN(_.toNumber(val)),
+    action: (val) => _.toNumber(val),
+  },
+  {
+    check: (val) => _.isNaN(_.toNumber(val)),
+    action: (val) => val,
+  },
+];
+
+const replacer = (key, val) => {
+  const { action } = typeActions.find((item) => item.check(val));
+  return action(val);
+};
+
+export default (ast) => JSON.stringify(ast, replacer);
