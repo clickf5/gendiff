d0baa9df87fb67d832ed09c0329bc9b12573b0aa
clickf5 <paninhome@gmail.com>
2020-02-06 10:31:21 +0300

    fix description

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,11 +5,11 @@ import gendiff from '../src';
 const extensions = ['json', 'yml', 'ini'];
 const outputs = ['tree', 'plain', 'json'];
 
-describe.each(extensions)('%s gendiff', (extension) => {
+describe.each(extensions)('input files extension: .%s', (extension) => {
   const beforeFilePath = path.resolve(`${__dirname}/../__fixtures__/before.${extension}`);
   const afterFilePath = path.resolve(`${__dirname}/../__fixtures__/after.${extension}`);
 
-  test.each(outputs)('%s', (output) => {
+  test.each(outputs)('output format: %s', (output) => {
     const result = fs.readFileSync(`${__dirname}/../__fixtures__/result_${output}.txt`, 'utf8').trim();
     expect(gendiff(beforeFilePath, afterFilePath, output)).toEqual(result);
   });
