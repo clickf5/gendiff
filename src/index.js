ac8e22fd501e1d21328427c5f03785277bcda200
clickf5 <paninhome@gmail.com>
2020-02-18 22:16:35 +0300

    to clean

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+import fs from 'fs';
+import path from 'path';
 import getParser from './parsers';
 import getRender from './formaters';
 import getAST from './buildAst';
 
-const gendiff = (beforeConf, afterConf, format) => {
+const gendiff = (beforeConfPath, afterConfPath, format) => {
   const parser = getParser();
-  const beforeObject = parser(beforeConf);
-  const afterObject = parser(afterConf);
-
+  const beforeConfData = fs.readFileSync(beforeConfPath, 'UTF-8');
+  const afterConfData = fs.readFileSync(afterConfPath, 'UTF-8');
+  const beforeConfType = path.extname(beforeConfPath).slice(1);
+  const afterConfType = path.extname(afterConfPath).slice(1);
+  const beforeObject = parser(beforeConfData, beforeConfType);
+  const afterObject = parser(afterConfData, afterConfType);
   const ast = getAST(beforeObject, afterObject);
-
   const render = getRender();
   return render(ast, format);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,5 @@
-import fs from 'fs';
 import yaml from 'js-yaml';
 import ini from 'ini';
-import path from 'path';
 
 const parsers = {
   json: JSON.parse,
@@ -9,7 +7,4 @@ const parsers = {
   ini: ini.parse,
 };
 
-export default () => (filepath) => {
-  const type = path.extname(filepath).slice(1);
-  return parsers[type](fs.readFileSync(filepath, 'utf-8'));
-};
+export default () => (data, type) => parsers[type](data);
