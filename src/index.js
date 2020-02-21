5257693b37ade8605a636296d5add4b99fe1d7a7
clickf5 <paninhome@gmail.com>
2020-02-21 09:08:14 +0300

    parser to parse

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import fs from 'fs';
 import path from 'path';
-import getParser from './parsers';
+import parse from './parsers';
 import getRender from './formaters';
 import getAST from './buildAst';
 
 const gendiff = (beforeConfPath, afterConfPath, format = 'tree') => {
-  const parser = getParser();
   const beforeConfData = fs.readFileSync(beforeConfPath, 'UTF-8');
   const afterConfData = fs.readFileSync(afterConfPath, 'UTF-8');
   const beforeConfType = path.extname(beforeConfPath).slice(1);
   const afterConfType = path.extname(afterConfPath).slice(1);
-  const beforeObject = parser(beforeConfData, beforeConfType);
-  const afterObject = parser(afterConfData, afterConfType);
+  const beforeObject = parse(beforeConfData, beforeConfType);
+  const afterObject = parse(afterConfData, afterConfType);
   const ast = getAST(beforeObject, afterObject);
   const render = getRender();
   return render(ast, format);
